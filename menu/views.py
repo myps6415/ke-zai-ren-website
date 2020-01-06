dc42b63b8c0042b0b4db6c9f1c6519ab0741d6d8
Tung Hsiao-Yu <myps6415@gmail.com>
2020-01-06 18:02:56 +0800

    little change hello page

--- menu/views.py
+++ menu/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
+from datetime import datetime
 
 
 # Create your views here.
 def hello_view(request):
     return render(request, 'hello_django.html', {
-        'data': "Hello Django",
+        'current_time': str(datetime.now()),
     })
