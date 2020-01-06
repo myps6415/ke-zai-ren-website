c279f9674353cde874fd2b0185434098bdf77565
Tung Hsiao-Yu <myps6415@gmail.com>
2020-01-06 16:31:59 +0800

    a little change

--- menu/views.py
+++ menu/views.py
@@ -4,5 +4,5 @@ from django.shortcuts import render
 # Create your views here.
 def hello_view(request):
     return render(request, 'hello_django.html', {
-        'data': "Hello Django ",
+        'data': "Hello Django",
     })
