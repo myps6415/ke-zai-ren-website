f7480f23d0becddecec40b64c3306cf8a80f0b0c
Tung Hsiao-Yu <myps6415@gmail.com>
2020-01-07 14:35:18 +0800

    add database

--- menu/models.py
+++ menu/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class menu(models.Model):
+    item = models.TextField(blank=False)
+    price = models.IntegerField(blank=False)
+    can_sell = models.BooleanField(blank=False)
